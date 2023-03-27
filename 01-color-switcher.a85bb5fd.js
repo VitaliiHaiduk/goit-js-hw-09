16c6c279a57f9278480c2ff9967479aa598749d3
VitaliiHaiduk <113601413+VitaliiHaiduk@users.noreply.github.com>
2023-03-27 18:50:43 +0000

    Deploying to gh-pages from @ VitaliiHaiduk/goit-js-hw-09@fc03611cc89195f8dde0672d6b41be1ff00fe8c1 🚀

---
+++ 01-color-switcher.a85bb5fd.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),e.setAttribute("disabled",!0),clearInterval(d)})),e.setAttribute("disabled",!0);let d=null;
+//# sourceMappingURL=01-color-switcher.a85bb5fd.js.map
